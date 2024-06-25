53446eb06d5d9e7716734a215d4078902bba2834
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-06-25 10:08:07 +0900

    sort 1

--- src/Uplus_Java_Class/sortkmp.java
+++ src/Uplus_Java_Class/sortkmp.java
@@ -1,4 +1,21 @@
 package Uplus_Java_Class;
 
+import java.util.Arrays;
+
 public class sortkmp {
+    public static void main(String[] args) {
+        int[] a = {30,20,590,1,2,49,4,10};
+        for(int i = 1; i < a.length; i++) {
+            int k = i;
+            for(int j = i-1; j >= 0; j--) {
+                if(a[j] > a[k]) {
+                    int T = a[j];
+                    a[j] = a[k];
+                    a[k] = T;
+                    k = j;
+                }
+            }
+            System.out.println(Arrays.toString(a));
+        }
+    }
 }

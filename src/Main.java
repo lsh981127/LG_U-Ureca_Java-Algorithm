c303de336d9a6ebdca569d50b91118e491f333f1
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-10 17:35:27 +0900

    Main 코드 수정

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        HashMap<Integer, Integer> map = new HashMap<>();
-//        map.
+        HashMap<String, Integer> comb = new HashMap<>();
+        String[][] clothes;
+        clothes = new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        String[] participant = new String[]{"eden", "kiki"};
+        int answer = 1;
+        HashMap<String, Integer> closet = new HashMap<>();
+        for(String[] cloth : clothes) {
+            closet.put(cloth[1], closet.getOrDefault(cloth[1], 0) + 1);
+            // 기존 replace 를 사용해서 값을 변경하려 했으나 put을 넣어야 동작하더라
+        }
+        for(Map.Entry<String, Integer> entry : closet.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
+        System.out.println(answer -1);
     }
 }

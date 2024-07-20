aeea724d266ee02ef2563badd85970fc22cac01b
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-20 17:01:20 +0900

    백준 30804 과일 탕후루 구현 중 1

---
+++ src/Uplus_Java_BaekJoon/bj_30804.java
@@ -0,0 +1,24 @@
+package Uplus_Java_BaekJoon;
+import java.util.*;
+import java.io.*;
+
+public class bj_30804 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+        br.close();
+    }
+
+
+}

d4d89249d553afa8ea9140d5b01ca469c2fad9fa
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-08-04 16:38:29 +0900

    2024.08.04

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("화이팅");
         System.out.println("집중해라");
+        System.out.println("공부에 집중하자!");
+
 
     }
 

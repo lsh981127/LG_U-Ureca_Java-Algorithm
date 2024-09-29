d94c8c486ffcc72007fece34e3f71c0b020ffacc
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-09-29 22:27:32 +0900

    2024.09.29

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("2024.09.28");
+        System.out.println("2024.09.29");
 
     }
 

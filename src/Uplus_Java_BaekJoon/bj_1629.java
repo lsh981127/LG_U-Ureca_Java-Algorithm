77c3f57c8483a15923d3f4129fa4e5478c8c6dd2
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2025-01-01 13:42:34 +0900

    2025.1.1

---
+++ src/Uplus_Java_BaekJoon/bj_1629.java
@@ -0,0 +1,33 @@
+package Uplus_Java_BaekJoon;
+
+import java.util.*;
+import java.io.*;
+
+public class bj_1629 {
+    static long A, B, C;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        C = Long.parseLong(st.nextToken());
+
+        System.out.println(pow(A, B));
+    }
+
+    static long pow(long value, long exp) {
+        if(exp == 1) return value % C;
+
+        long temp = pow(value, exp/2);
+        if(exp % 2 == 1) {
+            return (temp * temp % C) * value % C;
+        }
+
+        return temp * temp % C;
+
+    }
+
+
+
+}

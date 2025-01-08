38c76ec15043f0ecef52ef37b45458df1bc44855
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2025-01-08 12:27:00 +0900

    2025.1.8

--- src/Uplus_Java_BaekJoon/DFSandBFS/bj_13549.java
+++ src/Uplus_Java_BaekJoon/DFSandBFS/bj_13549.java
@@ -9,7 +9,7 @@ public class bj_13549 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        boolean[] visited = new boolean[200001];
+        boolean[] visited = new boolean[200001];  // 최대값이 100,000이기에 2배인 200,000까지 허용하기 위해 최대 Index를 200,000으로 잡았다
 
         Deque<int[]> queue = new ArrayDeque<>();
         int[] start = {N, 0};  // 시작, 현재 값
@@ -25,7 +25,10 @@ public class bj_13549 {
                 return;
             }
 
+            // 2배를 하는 연산이 최소값을 보장하기 때문에 제일 먼저 연산을 진행해야한다.
+            // 만약 2배 연산을 후순위에 두면 틀린다!
             int[] calc = {cur * 2, cur - 1, cur + 1};
+
             int[] time = {val, val + 1, val + 1};
 
             for(int i = 0; i < 3; i++) {

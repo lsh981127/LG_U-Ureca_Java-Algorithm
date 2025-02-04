c1e8a73d34803d5077ce364428dc0f0294d939de
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2025-02-03 22:53:08 -0500

    2025.2.3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("2024.2.3");
+        System.out.println("2024.2.3 ");
     }
 
 }
